447d5ea7dbaf3ec4b0902bb0d1bb9e1df706ed41
electron65 <57064558+electron65@users.noreply.github.com>
2020-06-07 01:51:19 +0900

    Create simple-boolean-test.cpp

---
+++ simple-boolean-test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	bool iLikePoets = true; // true is 1
+	bool iLikeMath = false; // false is 0
+
+	std::cout << "iLikePoets = " << iLikePoets << std::endl;
+	std::cout << "iLikeMath = " << iLikeMath << std::endl;
+
+	return 0;
+}
