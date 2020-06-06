841192320f0ac4122da05f8e2c838c05bfbb4d2e
electron65 <57064558+electron65@users.noreply.github.com>
2020-06-06 18:30:21 +0900

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	
+	std::cout << "Hello, World!" << std::endl;
+	
+	char input = 'C';
+
+	std::cin >> input;
+	std::cout << "input = " << input;
+
+	return 0;
+}
